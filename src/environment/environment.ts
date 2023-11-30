5eaadae8ad13ef32f699d376aa6060cc04df0d82
miguel12s <87570624+miguel12s@users.noreply.github.com>
2023-11-30 15:53:11 -0500

    commit

--- src/environment/environment.ts
+++ src/environment/environment.ts
@@ -1,6 +1,6 @@
 export const environment={
     production:false,
-    // apiUrl:"https://fastapi-dyl9-production.up.railway.app/"
-    apiUrl:"http://127.0.0.1:8000/"
+    apiUrl:"https://fastapi-dyl9-production.up.railway.app/"
+    // apiUrl:"http://127.0.0.1:8000/"
 
 }
